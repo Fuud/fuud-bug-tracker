6f5da152d7eefbb1ea79ffee12be3df486c28d31
Bobin Fedor <bobin@reksoft.ru>
2011-01-04 20:18:42 +0300

    small changes

--- src/main/java/com/blogspot/fuud/java/bugtracker/domain/Comment.java
+++ src/main/java/com/blogspot/fuud/java/bugtracker/domain/Comment.java
@@ -8,7 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-public class Comment extends User {
+public class Comment{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
